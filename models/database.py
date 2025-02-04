69a861ffa1736f3b5b869698158cd8a64161bbcc
ASW-Analyst <andy.wilson8@nhs.net>
2025-02-04 09:19:46 +0000

    Move DB to PostgreSQL and link to render

--- models/database.py
+++ models/database.py
@@ -1,12 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 # Define the SQLite database file location
-DATABASE_URL = "sqlite:///data/babynames.db"  # Save inside `data/`
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Create the database engine
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 
 # Session factory
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
