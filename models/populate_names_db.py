11023df1e394b1e6acfc67a75c357c5a40687c42
ASW-Analyst <andy.wilson8@nhs.net>
2025-02-05 11:20:08 +0000

    Trying to load as array

--- models/populate_names_db.py
+++ models/populate_names_db.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from models.database import SessionLocal
+from models.database import SessionLocal, engine
 from models.models import BabyName
 
 df_boys = pd.read_excel("data/boysnames2023.xlsx",
@@ -15,14 +15,15 @@ df_girls = pd.read_excel("data/girlsnames2023.xlsx",
 df_girls = df_girls.drop(columns=["Rank", "Count"])
 df_girls["Gender"] = "Girl"
 
-df_names = pd.concat([df_boys, df_girls], ignore_index=True)
-
+df_names = pd.concat([df_boys, df_girls])
+df_names_val = df_names.values
 # Open a database session
 db = SessionLocal()
 
 # Insert data
-for _, row in df_names.iterrows():
-    db.add(BabyName(name=row["Name"], gender=row["Gender"]))
+with engine.connect() as connection:
+    sql = "INSERT INTO names (name, gender) VALUES (%s, %s)"
+    connection.execute(sql, df_names_val.tolist())
 
 db.commit()
 db.close()
